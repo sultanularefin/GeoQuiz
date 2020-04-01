1b88b3cdc46505e41d759158ee3406ae5a74532b
unknown <sultanul.arefin@softbakso.com>
2020-04-01 16:27:33 +0600

    mvc ized the code

--- app/src/main/java/com/arefinsultan/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/arefinsultan/android/geoquiz/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 
@@ -15,7 +16,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
 
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
 
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+
+    private var currentIndex = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -30,6 +42,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+
+
+
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
+
+
 
         trueButton.setOnClickListener{view: View ->
             Toast.makeText(
@@ -57,7 +76,26 @@ class MainActivity : AppCompatActivity() {
 //                Toast.LENGTH_SHORT).setGravity(10,10,10)
 
         }
+
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+
+            updateQuestion()
+//            val questionTextResId = questionBank[currentIndex].textResId
+//            questionTextView.setText(questionTextResId)
+
+        }
+
+        updateQuestion()
+//        val questionTextResId = questionBank[currentIndex].textResId
+//        questionTextView.setText(questionTextResId)
     }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
 
 
 }
---
+++ app/src/main/java/com/arefinsultan/android/geoquiz/Question.kt
@@ -0,0 +1,6 @@
+package com.arefinsultan.android.geoquiz
+
+
+data class Question(@StringRes val textResId: Int, val answer: Boolean)
+//class Question {
+//}
